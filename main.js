a4991f9eb5c4d525ad5e0b9e9b59772e5648e6f0
Jussi Nummelin <jussi.nummelin@digia.com>
2015-08-17 14:11:00 +0300

    Changed Redis env variable name

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var port = process.env.PORT || 8080; // use value from environment variable or 8080 if not set
-var redisHost = process.env.REDIS_HOST || 'redis'
+var redisHost = process.env.REDIS_HOSTNAME || 'redis'
 var redisPort = process.env.REDIS_PORT || 6379
 var redis = require('redis');
 var r = redis.createClient(redisPort, redisHost); //creates a new client
@@ -11,7 +11,7 @@ http.createServer(function (req, res) {
       res.write('Hello World! I have been invoked ' + reply + ' times!\n');
       res.end();
   });
-    
+
 
 }).listen(port);
 console.log('The server is running! Listening connections at port ' + port);
